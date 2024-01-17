ce6eacfffe81eb86f27533ac2f191deffefccaca
ChoChiMoo <eddyojh@gmail.com>
2024-01-17 16:05:23 +0900

    Day3_prob2_fin

---
+++ programmers/python_workplace/stepByStep/Day3/strListToString.py
@@ -0,0 +1,9 @@
+#문자 리스트를 문자열로 변환하기 >>https://school.programmers.co.kr/learn/courses/30/lessons/181941
+#solution
+def solution(arr):
+    answer = ''.join(arr)
+    return answer
+
+#testcase
+
+#answer
