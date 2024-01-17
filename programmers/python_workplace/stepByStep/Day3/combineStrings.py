973e8023ef34954bc20d9ee04f035b3f35133541
ChoChiMoo <eddyojh@gmail.com>
2024-01-17 15:46:10 +0900

    Day3_prob1_fin

--- programmers/python_workplace/stepByStep/Day3/combineStrings.py
+++ programmers/python_workplace/stepByStep/Day3/combineStrings.py
@@ -1,7 +1,15 @@
 #문자열 섞기 >>https://school.programmers.co.kr/learn/courses/30/lessons/181942
-#solution1gzgz
+#solution
+
+
 def solution(str1, str2):
     answer = ''
+    for i in range(0, len(str1)):
+        answer = answer + str1[i] + str2[i]
     return answer
+
+stra, strb = map(str, input().strip().split(' '))
+
+print(solution(stra, strb))
 
 #answer
