270b45e360bb27f1de87da7c84ccdde448297f55
ChoChiMoo <hoon@OhJiHoonui-MacBookAir.local>
2024-01-21 16:32:04 +0900

    Day6_prob3_mid1

---
+++ programmers/python_workplace/stepByStep/Day6/handlingInt2.py
@@ -0,0 +1,11 @@
+#수 조작하기 2 >>https://school.programmers.co.kr/learn/courses/30/lessons/181925
+#solution
+def solution(numLog):
+    answer = ''
+    return answer
+
+#testcase=====================================
+
+#=============================================
+
+#answer
