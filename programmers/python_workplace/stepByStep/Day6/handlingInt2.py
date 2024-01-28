c1ade98dd7c1f1adc306e622067debd43c74d862
ChoChiMoo <hoon@OhJiHoonui-MacBookAir.local>
2024-01-28 16:16:32 +0900

    Day6_prob3_fin

--- programmers/python_workplace/stepByStep/Day6/handlingInt2.py
+++ programmers/python_workplace/stepByStep/Day6/handlingInt2.py
@@ -1,16 +1,21 @@
 #수 조작하기 2 >>https://school.programmers.co.kr/learn/courses/30/lessons/181925
 #solution
 def solution(numLog):
-    answer = []
+    answer = ''
+    key = { 1:'w', -1:'s', 10:'d', -10:'a'}
     for i in range(len(numLog)-1):
-        keyvalue = numLog[i + 1] - numLog[i]
-        key = { 1:'w', -1:'s', 10:'d', -10:'a'}
-        answer.append(key[keyvalue])
-    return ''.join(answer)
+        answer.append(key[numLog[i + 1] - numLog[i]])
+    return answer
 
 #testcase=====================================
 numLog = [0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1]
 print(solution(numLog))
 #=============================================
 
-#answer
+#answer1
+def solution(numLog):
+    answer = ''
+    key = { 1:'w', -1:'s', 10:'d', -10:'a'}
+    for i in range(len(numLog)-1):
+        answer += key[numLog[i + 1] - numLog[i]]
+    return answer
