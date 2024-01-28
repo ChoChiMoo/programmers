bff3492dc2a1548960ae64718c69a4907e8a1de7
ChoChiMoo <hoon@OhJiHoonui-MacBookAir.local>
2024-01-28 16:40:35 +0900

    Day6_prob4_mid1

---
+++ programmers/python_workplace/stepByStep/Day6/sequenceQuery3.py
@@ -0,0 +1,11 @@
+#수열과 구간 쿼리 3 >>https://school.programmers.co.kr/learn/courses/30/lessons/181924
+#solution
+def solution(arr, queries):
+    answer = []
+    return answer
+
+#testcase
+
+#===========================================
+
+#answer
